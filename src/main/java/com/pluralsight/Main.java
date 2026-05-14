eed7d3ae78a75abd6a450913f28838a81f899984
Chris Devalme <cdevalme@gmail.com>
2026-05-13 22:36:22 -0400

    - added user interface to main class

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,13 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DealershipFileManager dealership = new DealershipFileManager();
-        Dealership d2 = dealership.getDealership();
-        System.out.println(d2);
         UserInterface user = new UserInterface();
-
         user.display();
-
-
     }
 }
